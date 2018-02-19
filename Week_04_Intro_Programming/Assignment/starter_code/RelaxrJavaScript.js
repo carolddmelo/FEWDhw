b7cbeaa5de126d7c20dd4f22e7462880e04004fc
Carol <carolddmelo@gmail.com>
2018-02-19 17:22:02 -0500

    More Homework
    (HW 4)

--- Week_04_Intro_Programming/Assignment/starter_code/RelaxrJavaScript.js
+++ Week_04_Intro_Programming/Assignment/starter_code/RelaxrJavaScript.js
@@ -1,8 +1,46 @@
+jQuery(document).ready(function () {
+
+jQuery('.learnmore').click(function (e) {
+        e.preventDefault(); // --> COULD NOT FIGURE THE preventDefault alone
+        jQuery('#learnmoretext').slideDown();
+        jQuery('.learnmore').hide();
+    });
+    jQuery('.readmore').click(function (e) {
+        e.preventDefault();
+        jQuery('#show-this-on-click').slideDown();
+        jQuery('.readmore').hide();
+        jQuery('.readless').show();
+    });
+
+    jQuery('.readless').click(function (e) {
+        e.preventDefault();
+        jQuery('#show-this-on-click').slideUp();
+        jQuery('.readless').hide();
+        jQuery('.readmore').show();
+    });
+
+});
 
 
-// show more
-function slideDown(){
-	jQuery ('#show-this-on-click').slideDown ();
-	jQuery ('.readmore').hide();
-	jQuery ('.readless').show();
-}
+
+
+
+// PREVIOUS TRIAL (02/18)             
+//READ MORE
+//    function ReadMore (){
+//    jQuery('#show-this-on-click').slideDown();
+//    jQuery('.readless').hide();
+//    jQuery('.readmore').show()
+//}
+
+//jQuery ('#show-this-on-click').click (ReadMore)
+
+//LEARN MORE
+//function LearnMore (){
+//    jQuery('#learnmoretext').slideDown();
+//    jQuery('leanmore').hide();
+//}
+
+//jQuery (
+
+
